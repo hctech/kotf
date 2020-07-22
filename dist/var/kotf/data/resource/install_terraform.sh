1522a37004892b7c46f5c3022ed588ea6a9beb7d
chenyang <907693148@qq.com>
2020-07-22 10:46:39 +0800

    删除归档目录

--- dist/var/kotf/data/resource/install_terraform.sh
+++
@@ -1,2 +0,0 @@
-wget https://releases.hashicorp.com/terraform/0.12.28/terraform_0.12.28_linux_amd64.zip -O /tmp/terraform_0.12.28_linux_amd64.zip
-cd /tmp && unzip /tmp/terraform_0.12.28_linux_amd64.zip -d /build/kotf/
