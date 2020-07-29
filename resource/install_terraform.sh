898ab67e53cd7b81b03d13086c30bfbd5b16163d
KubeOperator <52770786+KubeOperator@users.noreply.github.com>
2020-07-29 17:34:47 +0800

    Update install_terraform.sh

--- resource/install_terraform.sh
+++ resource/install_terraform.sh
@@ -1,2 +1,3 @@
-wget https://releases.hashicorp.com/terraform/0.12.28/terraform_0.12.28_linux_amd64.zip -O /tmp/terraform_0.12.28_linux_amd64.zip
+# wget https://releases.hashicorp.com/terraform/0.12.28/terraform_0.12.28_linux_amd64.zip -O /tmp/terraform_0.12.28_linux_amd64.zip
+wget http://172.16.10.63/ko-3.0/data/kotf/terraform_0.12.28_linux_amd64.zip -O /tmp/terraform_0.12.28_linux_amd64.zip
 cd /tmp && unzip /tmp/terraform_0.12.28_linux_amd64.zip -d /build/kotf/
