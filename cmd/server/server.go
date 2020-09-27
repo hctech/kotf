06eab37256ff16d2a072b5553ebdaa3683e2b93d
wangzhengkun <zhengkun@fit2cloud.com>
2020-09-27 10:36:15 +0800

    feat(server): 增加传输和返回数据限制为100M

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -15,7 +15,11 @@ func newTcpListener(address string) (*net.Listener, error) {
 	return &s, nil
 }
 func newServer() *grpc.Server {
-	gs := grpc.NewServer()
+	var options = []grpc.ServerOption{
+		grpc.MaxRecvMsgSize(100 * 1024 * 1024),
+		grpc.MaxSendMsgSize(100 * 1024 * 1024),
+	}
+	gs := grpc.NewServer(options...)
 	kotf := server.NewKotf()
 	api.RegisterKotfApiServer(gs, kotf)
 	return gs
